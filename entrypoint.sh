62a55ac3e659f0944af1103cdd19969670615178
Alexey <373177@gmail.com>
2025-08-05 21:14:48 +0300

    fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ fi
 
 git config --global url."https://x-access-token:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
 
-if [ -z "$(ls -A /go/src/app)" ]; then
+if [ -z "$(ls -A root/go/src/app)" ]; then
    echo "Cloning repositories into /go/src/app..."
-   git clone https://github.com/autheo-network/go-sdk.git /go/src/app/go-sdk
-   git clone https://github.com/autheo-network/go-sdk-examples.git /go/src/app/go-sdk-examples
+   git clone https://github.com/autheo-network/go-sdk.git root/go/src/app/go-sdk
+   git clone https://github.com/autheo-network/go-sdk-examples.git root/go/src/app/go-sdk-examples
 else
    echo "Directory is not empty, skipping clone."
 fi
